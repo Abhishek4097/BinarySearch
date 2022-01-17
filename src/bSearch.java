10109b1eb4670d3a00b10270be8025cb151102ba
abhishek <abhishek.sharma_cs20@gla.ac.in>
2022-01-17 20:32:32 +0530

    Initial commit

---
+++ src/bSearch.java
@@ -0,0 +1,25 @@
+public class bSearch {
+    public static void main(String[] args) {
+        int[] arr={-12,-10,-8,0,2,5,6,12,34,65};
+        int target=12;
+        int ans=binarySearch(arr,target);
+        System.out.println(ans);
+    }
+    static int binarySearch(int[] arr,int target){
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(target<arr[mid]){
+                end=mid-1;
+            }
+            else if (target>arr[mid]){
+                start=mid+1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
